9d9526a6b3632fe58f202fac8cb83e8e42bac4a7
Sam <rieszein@gmail.com>
2022-10-20 23:23:04 -0500

    create proyect

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+import NavBar from "./components/nav";
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  );
+}
+
+export default App;
